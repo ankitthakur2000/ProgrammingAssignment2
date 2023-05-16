1c4b259c6af4ab0a5d1ca9622870daebfcae6b45
ankitthakur2000 <133707504+ankitthakur2000@users.noreply.github.com>
2023-05-16 10:02:45 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,39 +1,24 @@
-## These two functions, calculate the inverse of a square invertible matrix.The result will  
-## be cache so we can call it anytime we need it without recalculating it.
-
-## The makeCacheMatrix function creates a special matrix
-##It writes a list of functions to set the matrix and gets its value (cache the matrix). 
-##This function also sets the inverse matrix and gets its value (cache the inverse matrix).
-
-
-makeCacheMatrix<-function (x=matrix()){ 
-  i<-NULL                               
-  set<-function(y){                   ##Sets the value of the matrix, we assign the matrix to x        
-    x<<-y       
-    i<<-NULL
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
   }
-  get<-function()x                   ##Gets the value of the matrix x
-  setsolve<-function(solve)i<<-solve ##Sets the values of the matrix inversion 
-  getsolve<-function()i              ## Gets the values of the matrix inversion
-  list(set=set,get=get,setsolve=setsolve,getsolve=getsolve) 
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## cacheSolve calculates the inverse matrix of the CacheMatrix, and then computes it by subsetting
-## the elements of the list producen by makeCacheMatrix
-cacheSolve <- function(x, ...) {
-     ## Return a matrix that is the inverse of 'x'
-  i<-x$getsolve()
-  if(!is.null(i)){
+cacheinverse <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
     message("getting cached data")
-    return(i)
+    return(inv)
   }
-  data<-x$get()
-  i<-solve(data,...)
-  x$setsolve(i)
-  i
+  matrix_to_invert <- x$get()
+  inv <- solve(matrix_to_invert, ...)
+  x$setinverse(inv)
+  inv
 }
-  
-m1<-matrix(1:4,2,2)
-makeCacheMatrix(m1)
-cacheSolve(makeCacheMatrix(m1))
